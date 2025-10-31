dd41fca25f9155ebd14d506ae3060f8242343909
Thiago Macedo <thiagodark1995@gmail.com>
2025-10-31 14:58:25 -0400

    feat: :sparkles: add verification for user already exist by email

--- apps/server/src/controllers/User/user.service.ts
+++ apps/server/src/controllers/User/user.service.ts
@@ -2,11 +2,20 @@ import db from "@newsletter-io/db";
 import bcrypt from "bcryptjs";
 import { CreateUserDto } from "./dto/create.user.dto";
 import { GetUserDto } from "./dto/get.user.dto";
+import { BadRequestException } from "@/exceptions/BadRequestException";
 
 export default class UserService {
   constructor(private readonly model: typeof db.user = db.user) {}
 
   public async createUser(input: CreateUserDto): Promise<GetUserDto> {
+    const existingUser = await this.model.findUnique({
+      where: { email: input.email },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException("User with this email already exists.");
+    }
+
     const hashedPassword = await bcrypt.hash(input.password, 10);
 
     const user = await this.model.create({
