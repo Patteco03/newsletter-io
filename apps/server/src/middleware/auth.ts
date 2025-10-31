49e241ecdfd51fb22a526128d45794058e8e61b4
Thiago Macedo <thiagodark1995@gmail.com>
2025-10-31 17:42:43 -0400

    refactor: :recycle: refactor auth middleware

--- apps/server/src/middleware/auth.ts
+++ apps/server/src/middleware/auth.ts
@@ -1,26 +1,23 @@
+import { UnauthorizedException } from "@/exceptions/UnauthorizedException";
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-export function authMiddleware(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export function authMiddleware(req: Request, _: Response, next: NextFunction) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).json({ error: "No token provided" });
+    throw new UnauthorizedException("No token provided");
   }
 
   try {
     const token = authHeader.replace("Bearer ", "");
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
     if (!decoded) {
-      return res.status(401).json({ error: "Invalid token" });
+      throw new UnauthorizedException("Invalid token");
     }
 
     req.user = decoded as Express.UserPayload;
     next();
   } catch (error) {
-    return res.status(401).json({ error: "Invalid or expired token." });
+    throw new UnauthorizedException("Invalid or expired token.");
   }
 }
